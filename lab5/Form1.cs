ecd34c2a0de14ebe572add8372759f6fa748244f
Егор Гординский <yegorgordinskiy2002@gmail.com>
2022-10-06 16:48:54 +0800

    Добавьте файлы проекта.

---
+++ lab5/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pbMain_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pbMain_Paint(object sender, PaintEventArgs e)
+        {
+            var graphics = e.Graphics;
+            graphics.FillRectangle(new SolidBrush(Color.Yellow), 200, 100, 50, 30);
+            graphics.DrawRectangle(new Pen(Color.Red), 200, 100, 50, 30);
+        }
+    }
+}
